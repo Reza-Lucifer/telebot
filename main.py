534d1a451da272ca2b5a5ce7f9d23ca3b435784d
Reza-Lucifer <rkhdayy09@gmail.com>
2025-05-30 11:17:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import telebot
 from keep_alive import keep_alive
 
-TOKEN = os.getenv("7298261967:AAE6HqKW1m3KMZD8vpMR3y1MJn_p8SRa6Kc")
+TOKEN = os.getenv("TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
